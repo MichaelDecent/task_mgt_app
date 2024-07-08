ecfcae1575928a2cd6f21d6d9ebb7bc8e008e253
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-07-08 16:50:06 +0100

    fixed bug on delete user endpoint

--- src/main/java/com/michaeldecent/task_mgt_app/service/AuthService.java
+++ src/main/java/com/michaeldecent/task_mgt_app/service/AuthService.java
@@ -28,6 +28,7 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
 
     public UserResponseDTO createUser(UserRequestDTO request) {
+        System.out.println(request);
         var user = User.builder()
                 .firstName(request.getFirst_name())
                 .lastName(request.getLast_name())
@@ -43,6 +44,7 @@ public class AuthService {
 
 
     public AuthResponseDTO authenticateUser(AuthRequestDTO request) {
+        System.out.println(request.getEmail() + " and " + request.getPassword());
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
